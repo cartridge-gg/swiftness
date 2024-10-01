6b5bad86e19ef0ef997524db16be8d8e7803e662
Neo <neotheprogramist@proton.me>
2024-10-01 15:44:40 +0200

    in progress

--- proof_parser/src/stark_proof.rs
+++ proof_parser/src/stark_proof.rs
@@ -1,15 +1,17 @@
+use cainome_cairo_serde::CairoSerde;
+use cainome_cairo_serde_derive::CairoSerde;
 use num_bigint::BigUint;
 use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct StarkProof {
     pub config: StarkConfig,
     pub public_input: PublicInput,
     pub unsent_commitment: StarkUnsentCommitment,
     pub witness: StarkWitness,
 }
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct StarkConfig {
     pub traces: TracesConfig,
     pub composition: TableCommitmentConfig,
@@ -20,22 +22,22 @@ pub struct StarkConfig {
     pub log_n_cosets: u32,
     pub n_verifier_friendly_commitment_layers: u32,
 }
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct TracesConfig {
     pub original: TableCommitmentConfig,
     pub interaction: TableCommitmentConfig,
 }
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct TableCommitmentConfig {
     pub n_columns: u32,
     pub vector: VectorCommitmentConfig,
 }
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct VectorCommitmentConfig {
     pub height: u32,
     pub n_verifier_friendly_commitment_layers: u32,
 }
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct FriConfig {
     pub log_input_size: u32,
     pub n_layers: u32,
@@ -43,12 +45,12 @@ pub struct FriConfig {
     pub fri_step_sizes: Vec<u32>,
     pub log_last_layer_degree_bound: u32,
 }
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct ProofOfWorkConfig {
     pub n_bits: u32,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct StarkUnsentCommitment {
     pub traces: TracesUnsentCommitment,
     pub composition: BigUint,
@@ -57,24 +59,24 @@ pub struct StarkUnsentCommitment {
     pub proof_of_work: ProofOfWorkUnsentCommitment,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct TracesUnsentCommitment {
     pub original: BigUint,
     pub interaction: BigUint,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct FriUnsentCommitment {
     pub inner_layers: Vec<BigUint>,
     pub last_layer_coefficients: Vec<BigUint>,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct ProofOfWorkUnsentCommitment {
     pub nonce: BigUint,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct StarkWitness {
     pub traces_decommitment: TracesDecommitment,
     pub traces_witness: TracesWitness,
@@ -83,59 +85,59 @@ pub struct StarkWitness {
     pub fri_witness: FriWitness,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct TracesDecommitment {
     pub original: TableDecommitment,
     pub interaction: TableDecommitment,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct TableDecommitment {
     pub n_values: usize,
     pub values: Vec<BigUint>,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct TracesWitness {
     pub original: TableCommitmentWitness,
     pub interaction: TableCommitmentWitness,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct TableCommitmentWitness {
     pub vector: VectorCommitmentWitness,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct VectorCommitmentWitness {
     pub n_authentications: usize,
     pub authentications: Vec<BigUint>,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct TableCommitmentWitnessFlat {
     pub vector: VectorCommitmentWitnessFlat,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct VectorCommitmentWitnessFlat {
     pub n_authentications: usize,
     pub authentications: Vec<BigUint>,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct FriWitness {
     pub layers: Vec<FriLayerWitness>,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct FriLayerWitness {
     pub n_leaves: usize,
     pub leaves: Vec<BigUint>,
     pub table_witness: TableCommitmentWitnessFlat,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct PublicInput {
     pub log_n_steps: u32,
     pub range_check_min: u32,
@@ -152,13 +154,13 @@ pub struct PublicInput {
     pub continuous_page_headers: Vec<BigUint>,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct PubilcMemoryCell {
     pub address: u32,
     pub value: BigUint,
 }
 
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize, CairoSerde)]
 pub struct SegmentInfo {
     pub begin_addr: u32,
     pub stop_ptr: u32,
